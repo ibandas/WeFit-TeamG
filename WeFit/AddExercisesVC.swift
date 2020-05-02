8d47d33cfd521fad5b1935df62f2a06865104029
Irinel Bandas <ibandas@luc.edu>
2020-05-02 14:45:57 -0500

    Created lots of the structure for navigation to add exercises

---
+++ WeFit/AddExercisesVC.swift
@@ -0,0 +1,73 @@
+//
+//  AddExercisesVC.swift
+//  WeFit
+//
+//  Created by Irinel Bandas on 5/2/20.
+//  Copyright © 2020 Irinel Bandas. All rights reserved.
+//
+
+import UIKit
+
+class AddExercisesVC: UIViewController {
+    
+    @IBOutlet weak var possibleExercisesTV: UITableView!
+    
+    
+    @IBAction func AddSelectedExercises(_ sender: Any) {
+        var selectedExercises: [String] = []
+        let cells = self.possibleExercisesTV.visibleCells as! [PossibleExerciseCell]
+        for cell in cells {
+            if(cell.accessoryType == UITableViewCell.AccessoryType.checkmark) {
+                selectedExercises.append((cell.possibleExerciseTitle.text!))
+            }
+        }
+    }
+    
+    var exercises: [String] = ["Bench Press", "Squat", "Deadlift"]
+    var sectionsCount: Int = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        possibleExercisesTV.delegate = self
+        possibleExercisesTV.dataSource = self
+        print("Loaded")
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension AddExercisesVC: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exercises.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let possibleExerciseCell = self.possibleExercisesTV.dequeueReusableCell(withIdentifier: "PossibleExerciseCell", for: indexPath) as! PossibleExerciseCell
+        possibleExerciseCell.setPossibleExerciseTitle(title: exercises[indexPath.row])
+        return possibleExerciseCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (possibleExercisesTV.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark) {
+            possibleExercisesTV.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+        }
+        else {
+            possibleExercisesTV.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+    }
+}
--- WeFit/PossibleExerciseCell.swift
+++ WeFit/PossibleExerciseCell.swift
@@ -1,15 +1,21 @@
 //
-//  AddExerciseCell.swift
+//  PossibleExerciseCell.swift
 //  WeFit
 //
-//  Created by Irinel Bandas on 4/27/20.
+//  Created by Irinel Bandas on 5/2/20.
 //  Copyright © 2020 Irinel Bandas. All rights reserved.
 //
 
 import UIKit
 
-class AddExerciseCell: UITableViewCell {
+class PossibleExerciseCell: UITableViewCell {
 
+    @IBOutlet weak var possibleExerciseTitle: UILabel!
+    
+    func setPossibleExerciseTitle(title: String) {
+        possibleExerciseTitle.text = title
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,10 +25,6 @@ class AddExerciseCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-    }
-    
-    @IBAction func AddExercisesButton(_ sender: Any) {
-        
     }
 
 }
