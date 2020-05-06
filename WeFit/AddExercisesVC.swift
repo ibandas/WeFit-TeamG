48f8adbb99f37b8917e18c81c98689937c855a84
cristinabarclay <57467401+cristinabarclay@users.noreply.github.com>
2020-05-05 23:17:16 -0500

    ui, navigation, new challenge,

--- WeFit/AddExercisesVC.swift
+++ WeFit/AddExercisesVC.swift
@@ -10,17 +10,23 @@ import UIKit
 
 class AddExercisesVC: UIViewController {
     
+    @IBOutlet weak var searchBar: UITableView!
+
     @IBOutlet weak var possibleExercisesTV: UITableView!
     
     @IBOutlet weak var addButtonPressed: UIBarButtonItem!
     
-    var exercises: [String] = ["Bench Press", "Squat", "Deadlift"]
+    var exercises: [String] = ["Bench Press", "Squat", "Deadlift", "Pushup"]
     var selectedExercises: [Exercise] = []
+    var searchExercise = [String]()
+    var searching = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         possibleExercisesTV.delegate = self
         possibleExercisesTV.dataSource = self
+        searchBar.delegate = self
     }
     
     
@@ -36,18 +42,28 @@ class AddExercisesVC: UIViewController {
     }
 }
 
+
+
 extension AddExercisesVC: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching{
+            return searchExercise.count
+        } else{
         return exercises.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let possibleExerciseCell = self.possibleExercisesTV.dequeueReusableCell(withIdentifier: "PossibleExerciseCell", for: indexPath) as! PossibleExerciseCell
-        possibleExerciseCell.setPossibleExerciseTitle(title: exercises[indexPath.row])
+        if searching {
+            possibleExerciseCell.setPossibleExerciseTitle(title:searchExercise[indexPath.row])
+        } else{
+            possibleExerciseCell.setPossibleExerciseTitle(title: exercises[indexPath.row])
+        }
         return possibleExerciseCell
     }
     
@@ -59,4 +75,27 @@ extension AddExercisesVC: UITableViewDataSource, UITableViewDelegate {
             possibleExercisesTV.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
         }
     }
+}
+
+
+
+
+extension AddExercisesVC : UISearchBarDelegate{
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        searchExercise = exercises.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+        searching = true
+        possibleExercisesTV.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchBar.text = ""
+        possibleExercisesTV.reloadData()
+    }
+
+    
+    
 }
