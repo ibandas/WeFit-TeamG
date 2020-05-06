a5371ab3c0ffab52139e36f90ca92a789f86010a
Irinel Bandas <ibandas@luc.edu>
2020-05-05 20:29:29 -0500

    building off facebook login and learning about graph requests and permissions

--- WeFit/FacebookLoginVC.swift
+++ WeFit/FacebookLoginVC.swift
@@ -6,7 +6,7 @@ import UIKit
 import FBSDKLoginKit
 
 // Add this to the body
-class FacebookLoginVC: UIViewController {
+class FacebookLoginVC: UIViewController, LoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,6 +14,31 @@ class FacebookLoginVC: UIViewController {
         let loginButton = FBLoginButton()
         loginButton.center = view.center
         view.addSubview(loginButton)
+        
+        loginButton.delegate = self
+        loginButton.permissions = ["email", "public_profile"]
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBLoginButton!, didCompleteWith result: LoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        print("Successfully logged in with facebook...")
+        GraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start {
+            (connection, result, err) in
+            
+            if err != nil {
+                print("Failed to start graph request", err)
+                return
+            }
+            
+            print(result)
+        }
     }
 
 }
