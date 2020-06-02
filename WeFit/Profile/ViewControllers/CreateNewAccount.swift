99ff592f24ebd8378b61b56e48059c3f3434b498
Nicholas Trotter <ntrotter@Nicholass-MacBook-Pro-7.local>
2020-06-02 19:34:55 -0400

    added picture choosing code to email sign up (to avoid errors from missing picture, added button for it. Added alert if trying to create account without image

--- WeFit/Profile/ViewControllers/CreateNewAccount.swift
+++ WeFit/Profile/ViewControllers/CreateNewAccount.swift
@@ -17,10 +17,36 @@ class CreateNewAccount : UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var passwordConfirm: UITextField!
+    var pictureSet: bool = false;
+    
+    @IBAction func addImage(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    var newImage: UIImage
+
+    if let possibleImage = info[.editedImage] as? UIImage {
+        newImage = possibleImage
+    } else if let possibleImage = info[.originalImage] as? UIImage {
+        newImage = possibleImage
+    } else {
+        return
+    }
+
+    // do something interesting here!
+    User.sharedGlobal.profilePicture = newImage;
+    FacebookLoginVC().uploadImage(image: newImage, uid: User.sharedGlobal.uid);
+    dismiss(animated: true)
+        pictureSet = true;
+    }
     
 @IBAction func createAccount (_ sender: Any){
+    if pictureSet == false{
+        let alertController = UIAlertController(title: "Profile Picture Missing", message: "No Profile Picture Selected", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
     
-    if password.text != passwordConfirm.text {
+    else if password.text != passwordConfirm.text {
     let alertController = UIAlertController(title: "Password Incorrect", message: "Passwords do not match", preferredStyle: .alert)
     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                 
